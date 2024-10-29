322f6919dbb7824a2c664289708194ce3aadcb0f
Yoshiki Shin'ya <785431ys@gmail.com>
2024-10-29 15:26:30 +0900

    商品一覧画面と商品登録画面のロジックと画面作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -15,7 +16,9 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Items/Index', [
+            'items' => Item::select('id', 'name', 'price', 'is_selling')->get()
+        ]);
     }
 
     /**
@@ -25,7 +28,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +39,16 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+        return to_route('items.index')
+            ->with([
+                'message' => '登録しました。',
+                'status' => 'success'
+            ]);
     }
 
     /**
---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+  <div v-if="$page.props.flash.status === 'success'" class="bgblue-300 text-white p-4">
+    {{ $page.props.flash.message }}
+  </div>
+</template>
